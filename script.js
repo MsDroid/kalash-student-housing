16a4db452ab5d87e6a8664e516bc85dc0dc61a58
MsDroid <63984801+MsDroid@users.noreply.github.com>
2021-11-29 12:32:19 +0530

    video file update in landing page

---
+++ script.js
@@ -0,0 +1,33 @@
+ $(document).ready(function() {
+            $(".menu-icon").on("click", function() {
+                  $("nav ul").toggleClass("showing");
+            });
+});
+
+      // Scrolling Effect
+
+$(window).on("scroll", function() {
+      if($(window).scrollTop()) {
+            $('nav').addClass('black');
+      }
+
+      else {
+            $('nav').removeClass('black');            }
+})
+
+// 
+$(document).ready(function() {
+
+    $(window).scroll(function() {
+
+        var height = $('.first-container').height();
+        var scrollTop = $(window).scrollTop();
+
+        if (scrollTop >= height - 40) {
+            $('.nav-container').addClass('solid-nav');
+        } else {
+            $('.nav-container').removeClass('solid-nav');
+        }
+
+    });
+});
