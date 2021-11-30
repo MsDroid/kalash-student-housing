b676110ba17d349fa2551ed65b12d65906043191
MsDroid <63984801+MsDroid@users.noreply.github.com>
2021-11-30 16:18:54 +0530

    single page website done

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Kalash Student Housing - Best Student Housing in Mumbai</title>
+  <meta name="description" content="Best Student Housing Solution in Mumbai, JVPD, JUHU, Vile parle, Andheri Near NMIMS, Mithibai, UPG, Sardar Patel Engineering College, Raheja (Architectural college), Cooper Hospital Colleges. Get Best Rates">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="style.css" rel="stylesheet">
+  <link rel="shortcut icon" type="image/x-icon" href="logo.png">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" >
+</head>
+<body>
+	<header>
+  <div class="first-container">
+    <div class="nav-container">
+      <div class="nav-row">
+          <div class="nav-logo">
+            <a href="#">
+              <img src="logo.png" alt="logo" class="image" >
+            </a>
+          </div>
+          
+      </div>
+    </div>
+  </div>
+</header>
+
+<main>
+		<section class="showcase">
+			<div class="video-container">
+				<video src="http://kalashstudenthousing.com/kalashstudent.mp4" autoplay muted loop>
+				  
+				</video>
+			</div>
+			<div class="content">
+				<h1><b>Kalash Student Housing</b></h1>
+				<h3>Best Student Housing Solution.</h3>
+
+				
+              <a href="tel:+91-8298147755">
+              	<i class="fas fa-phone-alt call-icon" style="margin-right: 5px;"></i>91-8298147755
+              </a> | 
+              <a href="https://api.whatsapp.com/send/?phone=918298147755&amp;text=Hello,%20I%20came%20from%20website&amp;app_absent=0">
+              	<i class="fab fa-whatsapp whatsapp-icon" style="margin-right: 5px; color: greenyellow;"></i>91-8298147755
+              </a>
+          		<br>
+
+				<a href="tel:+91-8298147755" class="btn"><i class="fas fa-phone-alt call-icon" style="margin-right: 5px;"></i>Connect Now</a>
+			</div>
+		</section>
+
+		<section id="about">
+			<h1>About</h1>
+			<p>
+				Best Student Housing Solution in Mumbai, JVPD, JUHU, Vile parle, Andheri Near NMIMS, Mithibai, UPG, Sardar Patel Engineering College, Raheja (Architectural college), Cooper Hospital Colleges. Get Best Rates
+			</p>
+
+			<h2>Join Us On Social Media</h2>
+
+			<div class="social">
+			<a href="#" target="_blank"><i class="fab fa-twitter"></i></a>
+				<a href="#" target="_blank"><i class="fab fa-facebook"></i></a>
+				<!-- <a href="#" target="_blank"><i class="fab fa-github fa-3x"></i></a> -->
+				<!-- <a href="" target="_blank"><i class="fab fa-linkedin fa-3x"></i></a> -->
+			</div>
+	</section>
+</main>
+<script type="text/javascript" src="script.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/61a224ee53b398095a665a2f/1flgm0hjs';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+</body>
+</html>
